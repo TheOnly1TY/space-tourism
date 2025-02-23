1a73c732b1f596179e6d9fb1135856fd3946b496
TheOnly1TY <anifowosetemitayo5@gmail.com>
2025-02-23 20:48:02 +0100

    Add Router and implement basic routing  in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 export default function App() {
-  return <h1>SPACE TOURISM</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Homepage />} />
+        <Route path="destination" element={<Destination />} />
+        <Route path="crew" element={<Crew />} />
+        <Route path="technology" element={<Technology />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Homepage() {
+  return <h1>HOMEPAGE</h1>;
+}
+function Destination() {
+  return <h1>DESTINATION</h1>;
+}
+function Crew() {
+  return <h1>CREW</h1>;
+}
+function Technology() {
+  return <h1>TECHNOLOGY</h1>;
 }
