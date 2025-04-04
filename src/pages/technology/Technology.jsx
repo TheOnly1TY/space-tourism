0ae862b50f98fae7ed70594c0c7f447a4d24fc91
Temitayo <anifowosetemitayo5@gmail.com>
2025-04-04 23:26:46 +0100

    Update Technology.jsx

--- src/pages/technology/Technology.jsx
+++ src/pages/technology/Technology.jsx
@@ -3,7 +3,7 @@ import { TechnologyBody } from "./TechnologyBody";
 
 export function Technology() {
   return (
-    <div className="min-h-screen bg-[url(technology/background-technology-mobile.jpg)] md:bg-[url(technology/background-technology-tablet.jpg)]  lg:bg-[url(technology/background-technology-desktop.jpg)] bg-no-repeat bg-center bg-cover">
+    <div className="min-h-screen bg-[url(/technology/background-technology-mobile.jpg)] md:bg-[url(/technology/background-technology-tablet.jpg)]  lg:bg-[url(/technology/background-technology-desktop.jpg)] bg-no-repeat bg-center bg-cover">
       <NavBar />
       <TechnologyBody />
     </div>
