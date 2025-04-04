cb607a121395ff176400f23ea32ba7876ebe0015
Temitayo <anifowosetemitayo5@gmail.com>
2025-04-04 23:25:12 +0100

    Update Homepage.jsx

--- src/pages/home/Homepage.jsx
+++ src/pages/home/Homepage.jsx
@@ -3,7 +3,7 @@ import { HomeBody } from "./HomeBody";
 
 export function Homepage() {
   return (
-    <section className="min-h-screen bg-[url(home/background-home-mobile.jpg)] md:bg-[url(home/background-home-tablet.jpg)]  lg:bg-[url(home/background-home-desktop.jpg)] bg-no-repeat bg-center bg-cover">
+    <section className="min-h-screen bg-[url(/home/background-home-mobile.jpg)] md:bg-[url(/home/background-home-tablet.jpg)]  lg:bg-[url(/home/background-home-desktop.jpg)] bg-no-repeat bg-center bg-cover">
       <NavBar />
       <HomeBody />
     </section>
